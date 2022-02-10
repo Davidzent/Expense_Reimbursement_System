17cf9cddba09f57557e7a7575775e95dd63328c5
Davidgin <davidgin641@gmail.com>
2022-02-10 12:37:56 -0800

    Docker no connection issue fix
    Signed-off-by: Davidgin <davidgin641@gmail.com>

--- app/src/main/java/revature/App.java
+++ app/src/main/java/revature/App.java
@@ -3,14 +3,23 @@
  */
 package revature;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import revature.Controller.Controller;
+
+import static revature.util.ConnectionUtil.con;
 
 public class App {
 
     static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Controller con = new Controller();
+        try {
+            con.createStatement();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Controller conect = new Controller();
     }
 }
