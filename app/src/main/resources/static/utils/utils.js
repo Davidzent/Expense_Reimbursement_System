297882eb75a68b38bd07a9d9f1bec858aa4129cd
Davidgin <37416103+Davidzent@users.noreply.github.com>
2022-02-16 14:54:29 -0800

    Update utils.js

--- app/src/main/resources/static/utils/utils.js
+++ app/src/main/resources/static/utils/utils.js
@@ -1,4 +1,4 @@
-var URL = "http://localhost:7000";          //user site
+var URL = "http://35.193.86.50:7000";          //user site
 var REDIRURL = URL;
 
 // const URL = "http://35.193.86.50:7000";                              //online
