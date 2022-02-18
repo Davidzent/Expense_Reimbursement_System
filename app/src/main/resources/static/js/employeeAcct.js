e5c67f21c3579ae057803db2803297cc34739ac0
Davidgin <davidgin641@gmail.com>
2022-02-18 15:04:09 -0800

    Modal for update user
    Signed-off-by: Davidgin <davidgin641@gmail.com>

--- app/src/main/resources/static/js/employeeAcct.js
+++ app/src/main/resources/static/js/employeeAcct.js
@@ -1,12 +1,10 @@
 const emmpinfo = JSON.parse(localStorage.getItem('userinfo'));
 var tbl = document.getElementById('accountinfo');
 
-var editor = document.getElementById('accountEditor');
-var editbutton = document.getElementById('showEditor');
+var editbutton = document.getElementById('employeeupdate');
 
 
-editbutton.addEventListener('click', editAccount);
-editor.addEventListener('submit', submitInfo);
+editbutton.addEventListener('click', updateAcct);
 
 var acctNCell = document.createElement('tr')
 var nameCell = document.createElement('tr');
@@ -26,35 +24,3 @@ tbl.appendChild(nameCell);
 tbl.appendChild(lnameCell);
 tbl.appendChild(emailCell);
 tbl.appendChild(passCell);
-
-async function editAccount(event){
-    
-    event.preventDefault();
-    editor.innerHTML=`<label for='<label for='username'>Username</label>
-    <input id = 'username' name = 'username' type = 'text'/>
-    <label for='password'>password</label>
-    <input id = 'password' name = 'password' type = 'text'/>
-    <label for ='fname'>first name</label>
-    <input id = 'fname' name = 'fname' type = 'text'/>
-    <label for = 'lname'>last name</label> 
-    <input id = 'lname' name = 'lname' type = 'text'/>
-    <label for = 'email'>email</label>
-    <input id = 'email' name = 'email' type = 'text'/>
-    <input type='submit' id='submission' value='register'/>`
-    
-    
-}
-
-async function submitInfo(event){
-    event.preventDefault();
-    const formData = new FormData(this);
-        
-        ajax("post","/employee/update",formData).then(function (){
-            alert("you've updated");
-            document.getElementById('username').value="";
-            document.getElementById('password').value="";
-            document.getElementById('fname').value="";
-            document.getElementById('lname').value="";
-            document.getElementById('email').value="";
-        });
-}
